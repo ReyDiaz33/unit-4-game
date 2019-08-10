c794185dc3de6c44a86db978ded6cef159f103af
Rey Diaz <reybdiazjr@gmail.com>
2019-08-10 00:26:17 -0400

    Trying and failing. I can't find a way to interact with my classes.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,21 @@
+var goalNumber= [];
+var losses = 0;
+var wins = 0;
+var crystalNumber = [];
+
+function startGame(){
+    goalNumber = Math.floor(Math.random() * 99);
+    console.log(goalNumber);
+}
+
+
 /* Suedo coding
 - 4 crystals
 - every crystal has a random number between 1 - 12
 - a new radom number given to each crystal whether win or lose
 - clicking a crystal should add to previous click until total score
 - if less then goal number then continue.
-- if equal, increment win
-- if greater than goal, increment loss 
+- if equal to goal number, increment win
+- if greater than goal number, increment loss 
 */
+
